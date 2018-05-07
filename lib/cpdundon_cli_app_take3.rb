d76982b698bafd6d6312f881b8b697ebec948c78
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-07 03:14:36 -0400

    FINALLY GOT THE GEM UP AND RUNNINGgit status!

--- lib/cpdundon_cli_app_take3.rb
+++ lib/cpdundon_cli_app_take3.rb
@@ -3,9 +3,8 @@ require "cpdundon_cli_app_take3/version"
 module CpdundonCliAppTake3
   require 'gmt'
   require 'noaa_soap'
-  require 'noaa_pull_NYH'
   require 'get_water_level'
   require 'get_wind'
   require 'cli'
-#  require 'config'
+  require 'config'
 end
