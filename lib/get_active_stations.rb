26445dd785b92e7f8f1b22e9f30af7a44775c8ba
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-10 03:17:47 -0400

    finished for the day.  phase 2 adding multiple stations is shaping up nicely.  Will be complete with that very soon.

--- lib/get_active_stations.rb
+++ lib/get_active_stations.rb
@@ -1,33 +1,29 @@
 require_relative './config'
 
 class GetActiveStations
-	
+
+	FILE_PATH = "../run_support/responseStations.xml"	
+
 	@@doc = []
 	@@water_level = []
 	@@winds = []	
 
-	FILE_PATH = "../run_support/responseStations.xml"
-
-	def initialize
-		self.class.pull_data
-	end
-	
-	def self.doc
-		self.pull_data if @@doc == []
-		@@doc
-	end
-
 	def self.water_level
-		self.pull_data if @@wind_level == []		
+		self.pull_data if @@water_level.size == 0		
 		@@water_level
 	end
 
 	def self.winds
-		self.pull_data if @@winds == []		
+		self.pull_data if @@winds.size == 0		
 		@@winds
 	end
 
 private
+	def self.doc
+		@@doc
+	end
+
+
 	def self.pull_data
 		@@doc = []
 		@@water_level = []
@@ -35,16 +31,19 @@ private
 		
 		@@doc = File.open(FILE_PATH) { |f| Nokogiri::XML(f) }
 
-		stations = self.class.doc.xpath("//stationV2")		
+		stations = self.doc.xpath("//stationV2")		
 		stations.each_entry do |e|
+			iD = e.xpath(".").attribute("ID").value
+			name = e.xpath(".").attribute("name").value			
+
 			state = e.xpath("./metadataV2/location/state")[0].text
 			local = ((state == "NY") || (state == "NJ") || (state == "CT"))		
 						
 			water = e.xpath("./parameter[contains(@name, 'Water Level')]")
 			wind = e.xpath("./parameter[contains(@name, 'Winds')]")
 			
-			@@winds << e.xpath(".") if local && wind.size > 0
-			@@water_level << e.xpath(".") if local && water.size > 0	
+			@@winds << {:name => name, :iD => iD, :state => state} if local && wind.size > 0
+			@@water_level << {:name => name, :iD => iD, :state => state} if local && water.size > 0	
 		end
 	end	
 end
